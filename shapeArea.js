901f639f749cb6f36b39bcbfd82b2f1c7588661c
Anthony Ruffin <anthonyruffin2010@gmail.com>
2017-11-02 21:36:37 -0400

    added shape area algorithm

---
+++ shapeArea.js
@@ -0,0 +1,27 @@
+function shapeArea(numberOfSquares) {
+    let area = 
+
+    return area;
+}
+
+let displayMe = shapeArea(3);
+console.log(displayMe);
+
+/*
+x = 2; y = 5
+x = 3; y = 13
+x = 4; y = 26;
+
+Everytime you add a square you are increasing by x-times
+
+diameter is equal to n * 2 - 1; Example: n = 2 | diameter = 3;
+
+
+*/
+
+function shapeArea(n) {
+    let area = n*n + (n-1)*(n-?);
+}
+
+
+
