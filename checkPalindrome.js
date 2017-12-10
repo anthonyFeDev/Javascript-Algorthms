71c8536f7e345d0eb64476816d8e1d3b0d8337fc
Anthony Ruffin <anthonyruffin2010@gmail.com>
2017-12-10 18:51:40 -0500

    added an improved solution

--- checkPalindrome.js
+++ checkPalindrome.js
@@ -1,13 +1,27 @@
-function checkPalindrome(inputString) {
-    var reversedString = inputString.split('').reverse().join('');
-    
-    console.log(reversedString);
+//Solution 1
 
-    if (inputString == reversedString){
-        return true;
-    } else {
-        return false;
-    }
+// function checkPalindrome(inputString) {
+//     var reversedString = inputString.split('').reverse().join('');
+    
+//     console.log(reversedString);
+
+//     if (inputString == reversedString){
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+//[Improved] Solution 1
+// function checkPalindrome(str){
+//     let reverse = str.split('').reverse().join('');
+
+//     return str === reverse;
+// }
+
+//Solution 2
+function checkPalindrome(str){
+    
 }
 
 checkPalindrome("abac");
