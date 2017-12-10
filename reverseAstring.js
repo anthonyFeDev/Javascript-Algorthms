8986f17462ec16119e9dab6c5690e1237610a620
Anthony Ruffin <anthonyruffin2010@gmail.com>
2017-12-10 18:15:13 -0500

    improved reverse algorithm \

--- reverseAstring.js
+++ reverseAstring.js
@@ -1,13 +1,17 @@
-function reverseString(str){
+// function reverseString(str){
     
-    var splitString = str.split("");
-    var stringArray = [];
-    stringArray = splitString;
+//     var splitString = str.split("");
+//     var stringArray = [];
+//     stringArray = splitString;
 
-    var newString = stringArray.reverse().join("");
+//     var newString = stringArray.reverse().join("");
 
 
-    return newString;
+//     return newString;
+// }
+
+function reverseString(str){
+    return str.split('').reverse().join('');
 }
 
 reverseString("hello");
