e2385519e809ff04609b8b55e07f84a2f90fd882
Anthony Ruffin <anthonyruffin2010@gmail.com>
2017-12-10 22:04:44 -0500

    added maxChar.js

---
+++ maxChar.js
@@ -0,0 +1,14 @@
+
+function maxChar(str){
+    let charMap = {};
+
+    for (let char of str){
+        if(charMap[char]){
+            charMap[char]++;
+        } else charMap[char] = 1;
+    }
+
+    console.log(charMap);
+}
+
+maxChar("Jelly Bears");
